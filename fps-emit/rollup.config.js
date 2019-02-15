387f47c092aebe31bfa22d2599adf3d66262811e
Jan Polacek <jan.polacek@nike.sk>
2019-02-15 13:38:37 +0100

    updated versions

--- fps-emit/rollup.config.js
+++ fps-emit/rollup.config.js
@@ -2,7 +2,7 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
+import { terser } from "rollup-plugin-terser";
 
 const env = process.env.NODE_ENV
 
@@ -24,19 +24,10 @@ const config = {
     }),
     commonjs()
   ]
-}
+};
 
 if (env === 'production') {
-  config.plugins.push(
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false
-      }
-    })
-  )
+  config.plugins.push(terser())
 }
 
 export default config
